e9feb919230892d3a3adb5897498337fa36c8616
Antonia Luciana Pires <antonialucianapires@gmail.com>
2022-02-08 22:18:10 -0300

    refactor: divisão de responsabilidades

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,41 +1,11 @@
-const criarNovaLinha = (nome, email) => {
-
-    const linhaNovoCliente = document.createElement('tr');
-
-    const conteudo = `
-
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td> 
-    `
-
-    linhaNovoCliente.innerHTML = conteudo;
-    return linhaNovoCliente;
-}
-
-const tabela = document.querySelector('[data-tabela');
-
-
 const listaClientes = () => {
 
-    return fetch(`http://localhost:3000/profile`)
+    return fetch(`http://localhost:3001/profile`)
         .then(resposta => {
             return resposta.json()
         })
 }
 
-listaClientes()
-    .then(data => {
-        console.log(data)
-        data.forEach(cliente => {
-            tabela.appendChild(criarNovaLinha(cliente.nome, cliente.email));
-        })
-
-    });
-
-
+export const clienteService = {
+    listaClientes
+}
